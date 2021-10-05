3d11bdb7731e11d907fcb5f385397e6cf28a11e8
AndLup <90223794+AndLup@users.noreply.github.com>
2021-10-05 16:19:58 -0500

    Add files via upload

---
+++ MediaQueries-AndresLupian/js/main.js
@@ -0,0 +1,15 @@
+const typed = new Typed('.typed', {
+    strings: ['', 'Aprende Diseño Web Moderno'],
+    
+    
+    typeSpeed: 100,
+    startDelay: 300,
+    backSpeed: 100,
+    smartBackspace: true,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|'
+
+});
+
